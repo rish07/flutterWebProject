ae539f36dc00f1679d081dc4c5127713cafbe48c
Creator <rishirajsinghchhabra@gmail.com>
2020-05-02 21:47:10 +0530

    Basic

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/responsive_widget.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -8,10 +9,67 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title:Text('working'),
-        centerTitle: true,
+    return ResponsiveWidget(
+      largeScreen: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          elevation: 0,
+          title: Text('working'),
+          centerTitle: true,
+        ),
+        drawer: ResponsiveWidget.isSmallScreen(context)
+            ? Drawer(
+                child: ListView(
+                padding: EdgeInsets.all(20),
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage('hero-bg.jpg'),
+                  )
+                ],
+              ))
+            : null,
+        body: SingleChildScrollView(
+          child: AnimatedPadding(
+            padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.1),
+            duration: Duration(seconds: 1),
+            child: ResponsiveWidget(
+              largeScreen: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  NavHeader(),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.1,
+                  ),
+                  Text(
+                    'working',
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class NavHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveWidget(
+      largeScreen: Row(
+        mainAxisAlignment: ResponsiveWidget.isSmallScreen(context)
+            ? MainAxisAlignment.center
+            : MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          if (!ResponsiveWidget.isSmallScreen(context))
+            Row(
+              children: <Widget>[],
+            )
+        ],
       ),
     );
   }
